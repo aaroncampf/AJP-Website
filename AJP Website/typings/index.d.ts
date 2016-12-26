37351522c7565517ff8f97d42eb5b6815a17a1cb
Aaron Campf <aaroncampf@gmail.com>
2016-12-26 10:20:20 -0800

    Added stuff

--- AJP Website/typings/index.d.ts
+++ AJP Website/typings/index.d.ts
@@ -1,3 +1,4 @@
-/// <reference path="modules/node/index.d.ts" />
 /// <reference path="modules/express-serve-static-core/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/node/index.d.ts" />
+/// <reference path="modules/jquery/index.d.ts" />
