eec971127df9a65a7ed25351d71ca6678095a182
Aaron Campf <aaroncampf@gmail.com>
2016-12-25 17:34:36 -0800

    Added more models

--- AJP Website/Scripts/Model_Classes.js
+++ AJP Website/Scripts/Model_Classes.js
@@ -2,6 +2,11 @@ class CATG extends Array {
 }
 class Group extends Array {
 }
-class Product {
+class Product extends Array {
+}
+/**
+ * A special price given to a customer for an item
+ */
+class SP {
 }
 //# sourceMappingURL=Model_Classes.js.map
--- AJP Website/Scripts/Model_Classes.ts
+++ AJP Website/Scripts/Model_Classes.ts
@@ -9,12 +9,25 @@ class Group extends Array<Object> {
 	public Product: Array<Product>;
 }
 
-class Product {
+class Product extends Array<SP> {
+	/** The ID of the Record */
 	public ID: string;
+	/** The Name of the item */
 	public Name: string;
+	/** The unit it was sold in */
 	public Case: string;
+
+	/** The ID number of the item's vendor that will be used in place of the name until further notice */
 	public VenderID: string;
+	/** The purchase cost (I think) */
 	public LINECOST: string;
+	/** The */
 	public SELL1: string;
 	public SELL6: string;
+}
+
+/**
+ * A special price given to a customer for an item
+ */
+class SP {
 }
